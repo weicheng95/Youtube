1acf9a434385ae55dc4c219ff532b0b21c906873
weicheng <weicheng@weichengs-MacBook-Air.local>
2018-08-25 18:06:47 +0800

    update 4

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 //         }
 //     }, (error) => console.log(err));
 
-
-app.listen(3000, function () {
+axios.get('')
+app.listen(process.env.PORT || 3000, function () {
     console.log('listening on port 3000')
 })
