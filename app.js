f3c5922c0b75e450493224da1fc9fe012d488bce
weicheng <weicheng@weichengs-MacBook-Air.local>
2018-08-26 17:12:37 +0800

    update 5

--- app.js
+++ app.js
@@ -3,28 +3,47 @@ const app = express()
 const cheerio = require('cheerio');
 const axios = require('axios')
 const fs = require('fs');
+const lyrics = require('./lyrics')
+const bodyParser = require('body-parser');
+
+// lyrics.getLyrics('something just like this', 'chainsmokers')
+// .then((lyric) => {
+//     console.log('from outside:')
+//     console.log(lyric)
+//     lyricsFromAz = lyric
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
 
 //view engine
 app.set('view engine', 'ejs')
+
 //import public
 app.use(express.static('static'))
+
+app.use(bodyParser())
 
 app.get('/', function (req, res) {
     res.render('index')
 })
 
-// axios
-//     .get('https://dev.to/aurelkurtula')
-//     .then((response) => {
-//         if (response.status === 200) {
-//             const html = response.data;
-//             const $ = cheerio.load(html);
+app.post('/getlyrics', function(req, res) {
+    console.log(req.body)
+    var title = req.body.title
+    var author = req.body.author
 
-//             console.log($.text())
-//         }
-//     }, (error) => console.log(err));
+    lyrics.getLyrics(title, author)
+    .then((lyric) => {
+        return res.send(lyric)
+    })
+    .catch((err) => {
+        console.log(err)
+        return res.send('false')
+    })
 
-axios.get('')
+})
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('listening on port 3000')
 })
---
+++ lyrics.js
@@ -0,0 +1,73 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const searchLyricsBase = 'https://search.azlyrics.com/search.php';
+const lyricsBase = 'https://azlyrics.com/';
+const h2p = require('html2plaintext')
+// const transformMsg = msg => {
+//     const arr = msg.split(/\s*\-\s*/g);
+//     if(arr.length !== 2) return false;
+//     return arr;
+// }
+
+const transformLink = data => {
+	data = data.split(' ',).join('+');	
+	data = data.toLowerCase();
+	return data;
+}
+
+const lyricsUrl = (title,author) => {
+	let returnUrl = searchLyricsBase;	
+	author = transformLink(author);
+    title = transformLink(title);
+    //search.php?q=the+chainsmokers+something+just+like+this
+    //returnUrl += `lyrics/${author}/${title}.html`;
+    console.log({title})
+    console.log({author})
+    //returnUrl += `?q=${author}+${title}`;
+    returnUrl += `?q=${title}`;
+    console.log({returnUrl})
+	return returnUrl;
+}
+
+// const lyrics = (url) => {
+//     axios.get(url).then(res => {
+//         if(res.status === 200) {
+//             const $ = cheerio.load(res.data)
+//             $('.col-xs-12.col-lg-8.text-center').each(function(){
+//                 let lyrics = h2p($(this).html()).split('Submit Corrections')[0]
+//                 console.log(lyrics)
+//                 return lyrics
+//             })
+//         }
+//     })
+// }
+
+exports.getLyrics = (title, author) => {
+	return new Promise((resolve, reject) => {
+        const URI = lyricsUrl(title, author);
+        axios.get(URI)
+		.then(res => {
+            const $ = cheerio.load(res.data)
+            console.log($)
+            if($('.alert.alert-warning').length > 0){
+                return resolve('no result found')
+            }
+            $('td.text-left a').each(function(){
+                url = $(this).attr('href');
+                // Get Lyrics
+                axios.get(url).then(res => {
+                    if(res.status === 200) {
+                        const $ = cheerio.load(res.data)
+                        $('.col-xs-12.col-lg-8.text-center').each(function(){
+                            let lyrics = $(this).html().split('Submit Corrections')[0]
+                            console.log(lyrics)
+                            return resolve(lyrics)
+                        })
+                    }
+                })
+                return false;
+            });
+		})
+		.catch(err => reject(`You're doing it wrong!\nServer responded with status ${err.statusCode === 404 ? err.statusCode + ' not found!' : err.statusCode}\n\nFormat: Artist - Song.\nAdditional hyphens in the title should be omitted`));
+	});
+}
